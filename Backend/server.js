0895008760c8f43c14930b63555f79c38f904d1c
Ravi Mohan <its.ravimohan01@gmail.com>
2026-01-03 11:12:20 +0530

    for deployement

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 // CORS
 app.use(
   cors({
-    origin: ["http://127.0.0.1:5500", "http://localhost:5500", "http://127.0.0.1:5501"],
+    origin: ["sdb-7lm.pages.dev"],
+    credentials : true
   })
 );
 
